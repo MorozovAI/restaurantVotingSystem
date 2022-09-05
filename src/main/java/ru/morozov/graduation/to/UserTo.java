fd998b06d3ee74ee021db15640a17d4ce61881b4
MorozovAI <m407345@mail.ru>
2022-09-05 15:33:48 +0300

    little refactoring

--- src/main/java/ru/morozov/graduation/to/UserTo.java
+++ src/main/java/ru/morozov/graduation/to/UserTo.java
@@ -22,7 +22,7 @@ public class UserTo extends NamedTo implements HasIdAndEmail {
     @Size(min = 5, max = 32)
     String password;
 
-    public UserTo(Integer id, String name, String email, String password, int caloriesPerDay) {
+    public UserTo(Integer id, String name, String email, String password) {
         super(id, name);
         this.email = email;
         this.password = password;
